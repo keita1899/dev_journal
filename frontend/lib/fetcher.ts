927d3ec1379d938cfb1a55fc36c7796f33ebc6af
keita1899 <saboten18k@gmail.com>
2025-08-12 06:54:42 +0900

    chore: axiosとswrをインストールして、必要な関数とインターセプターを作成

---
+++ frontend/lib/fetcher.ts
@@ -0,0 +1,9 @@
+import axiosInstance from './axios'
+
+/**
+ * SWRに渡すためのfetcher関数
+ * @param url APIのエンドポイントのパス
+ * @returns 取得したデータ (JSON)
+ */
+export const fetcher = (url: string) =>
+  axiosInstance.get(url).then((res) => res.data)
