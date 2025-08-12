2f1b4266dd7e699dbea152499c194c8c225d0631
keita1899 <saboten18k@gmail.com>
2025-08-13 06:50:14 +0900

    feat: ApplicationControllerにエラーハンドリングとJSON形式を強制させるコードを追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+
+  before_action :ensure_json_request
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { error: e.message }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 end
