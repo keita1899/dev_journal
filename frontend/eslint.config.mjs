76fdd5c1115d6dc5513aac606851386f7afbca4d
keita1899 <saboten18k@gmail.com>
2025-08-12 18:07:02 +0900

    chore: ESLintとPrettierのセットアップ

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,71 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettierConfig from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import tailwindcssPlugin from 'eslint-plugin-tailwindcss'
+import jestDomPlugin from 'eslint-plugin-jest-dom'
+import testingLibraryPlugin from 'eslint-plugin-testing-library'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 
-export default eslintConfig;
+  {
+    plugins: {
+      tailwindcss: tailwindcssPlugin,
+    },
+    rules: {
+      ...tailwindcssPlugin.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+    plugins: {
+      'jest-dom': jestDomPlugin,
+      'testing-library': testingLibraryPlugin,
+    },
+    rules: {
+      ...jestDomPlugin.configs.recommended.rules,
+      ...testingLibraryPlugin.configs.react.rules,
+    },
+  },
+
+  prettierConfig,
+]
+
+export default eslintConfig
