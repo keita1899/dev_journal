bc1aff9e5383a827a178c6526d02c785aee5ff58
keita1899 <saboten18k@gmail.com>
2025-08-13 19:07:36 +0900

    fix: ensure_json_requestが未定義だったので作成

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::API
 
   before_action :ensure_json_request
 
+  private
+
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: { error: e.message }, status: :not_found
   end
 
   rescue_from ActionController::ParameterMissing do |e|
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def ensure_json_request
+    request.format = :json
   end
 end
