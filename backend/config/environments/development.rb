cde4aedcb0d2b9df52bbc3a3ba8b031f48d13307
keita1899 <saboten18k@gmail.com>
2025-09-16 20:27:05 +0900

    feat: letter_openerを導入し、開発環境のメール送信を確認できるようにする
    - Gemを追加
    - development環境の設定を追記
    - ルーティングを追加

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3300 }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 end
