70669bac7988863d2725d64dcaaab44eb1d75eb9
keita1899 <saboten18k@gmail.com>
2025-09-22 22:57:27 +0900

    feat: ユーザー新規登録APIを実装
    - `Api::V1::UsersController`、`create`アクションを作成
    - `api/v1/users`へのルーティングを作成
    - デフォルトのパスワードを6文字から8文字以上に設定

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, length: { maximum: 255 }
+  validates :password, length: { maximum: 128 }, if: -> { new_record? || password.present? }
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+    end
+  end
   devise_for :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
