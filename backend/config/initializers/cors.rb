b88d720771222de800b2405a51e577955408268e
keita1899 <saboten18k@gmail.com>
2025-09-30 08:36:14 +0900

    chore(cors): credentialsを削除して認証ヘッダーをexposeに追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -12,6 +12,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
-             credentials: true
+             expose: %w[access-token expiry token-type uid client]
   end
 end
