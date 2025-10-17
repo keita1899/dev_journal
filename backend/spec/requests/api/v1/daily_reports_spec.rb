c9c36e8ebe75f83b89183bc38e7d10ef59111168
keita1899 <saboten18k@gmail.com>
2025-10-17 21:51:27 +0900

    test: DailyReportsControllerのindexアクションのリクエストスペックを追加

--- backend/spec/requests/api/v1/daily_reports_spec.rb
+++ backend/spec/requests/api/v1/daily_reports_spec.rb
@@ -1,10 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::DailyReports', type: :request do
-  describe 'POST /api/v1/daily_reports' do
-    let!(:user) { create(:user, email: 'test@example.com', password: 'password123') }
-    let(:headers) { user.create_new_auth_token }
+  let!(:user) { create(:user, email: 'test@example.com', password: 'password123') }
+  let(:headers) { user.create_new_auth_token }
 
+  describe 'GET /api/v1/daily_reports' do
+    let(:params) { { year: '2025', month: '10' } }
+
+    context 'ログインしている場合' do
+      context '日報のデータが存在する場合' do
+        it 'okステータス(200)が返されること' do
+          get api_v1_daily_reports_path, params:, headers: headers
+          expect(response).to have_http_status(:ok)
+        end
+
+        it '自分の日報のデータが返されること' do
+          own_report = create(:daily_report, user: user, date: Date.new(2025, 10, 5))
+          get api_v1_daily_reports_path, params:, headers: headers
+
+          expect(json.length).to eq(1)
+          expect(json.first['id']).to eq(own_report.id)
+        end
+
+        it '他人の日報のデータは返されないこと' do
+          other_user = create(:user)
+          other_report = create(:daily_report, user: other_user, date: Date.new(2025, 10, 10))
+          get api_v1_daily_reports_path, params:, headers: headers
+
+          ids = json.map { |daily_report| daily_report['id'] }
+          expect(ids).not_to include(other_report.id)
+        end
+      end
+
+      context '日報のデータが存在しない場合' do
+        it 'okステータス(200)が返されること' do
+          get api_v1_daily_reports_path, params:, headers: headers
+          expect(response).to have_http_status(:ok)
+        end
+
+        it '空の配列が返ること' do
+          get api_v1_daily_reports_path, params:, headers: headers
+          expect(json).to eq([])
+        end
+      end
+
+      context '日付が不正な場合' do
+        let(:params) { { year: 'abcd', month: '13' } }
+
+        it '400 Bad Requestが返ること' do
+          get api_v1_daily_reports_path, params:, headers: headers
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'unauthorizedステータス(401)が返されること' do
+        get api_v1_daily_reports_path
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'POST /api/v1/daily_reports' do
     context 'ログインしている場合' do
       context '有効なパラメータの場合' do
         let(:params) do
