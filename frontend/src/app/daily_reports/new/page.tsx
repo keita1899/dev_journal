eb6e611bc90a651a4adcee578139580a44505ea5
keita1899 <saboten18k@gmail.com>
2025-10-19 22:40:42 +0900

    fix: SuspenseでDailyReportWrapperをラップしてuseSearchParamsエラーを解消

--- frontend/src/app/daily_reports/new/page.tsx
+++ frontend/src/app/daily_reports/new/page.tsx
@@ -1,33 +1,17 @@
 'use client'
 
-import { format, isValid, parseISO } from 'date-fns'
-import { useSearchParams } from 'next/navigation'
-import { Suspense, useEffect } from 'react'
-import toast from 'react-hot-toast'
+import { Suspense } from 'react'
 
-import { DailyReportEditor } from '@/features/dailyReport/components/DailyReportEditor'
+import { DailyReportWrapper } from '@/features/dailyReport/components/DailyReportWrapper'
 import { useRedirectIfUnauthenticated } from '@/hooks/useRedirectIfUnauthenticated'
 
 const DailyReportNewPage = () => {
   useRedirectIfUnauthenticated()
-  const searchParams = useSearchParams()
-
-  const dateParam = searchParams.get('date')
-  const today = format(new Date(), 'yyyy-MM-dd')
-
-  const parsed = dateParam ? parseISO(dateParam) : null
-  const date = dateParam && isValid(parsed!) ? dateParam : today
-
-  useEffect(() => {
-    if (parsed && !isValid(parsed)) {
-      toast.error('不正な日付が指定されました。')
-    }
-  }, [parsed])
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <DailyReportEditor date={date} content="" />
+    <Suspense>
+      <DailyReportWrapper />
     </Suspense>
   )
 }
+
 export default DailyReportNewPage
---
+++ frontend/src/features/dailyReport/components/DailyReportWrapper.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { format, isValid, parseISO } from 'date-fns'
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import toast from 'react-hot-toast'
+
+import { DailyReportEditor } from './DailyReportEditor'
+
+export const DailyReportWrapper = () => {
+  const searchParams = useSearchParams()
+  const dateParam = searchParams.get('date')
+  const today = format(new Date(), 'yyyy-MM-dd')
+  const parsed = dateParam ? parseISO(dateParam) : null
+  const date = dateParam && isValid(parsed!) ? dateParam : today
+
+  useEffect(() => {
+    if (parsed && !isValid(parsed)) {
+      toast.error('不正な日付が指定されました。')
+    }
+  }, [parsed])
+
+  return <DailyReportEditor date={date} content="" />
+}
