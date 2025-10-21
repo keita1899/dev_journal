b61827bd29c2a9621516d3dec1ee9a4b1822c531
keita1899 <saboten18k@gmail.com>
2025-10-21 17:03:27 +0900

    feat: UserモデルとArticleモデルを１対多の関係にするアソシエーションを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :daily_reports, dependent: :destroy
+  has_many :articles, dependent: :destroy
 
   validates :email, length: { maximum: 255 }
   validates :password, length: { maximum: 128 }, if: -> { new_record? || password.present? }
