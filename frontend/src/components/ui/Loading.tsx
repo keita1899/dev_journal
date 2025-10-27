0fe54d14d3c427188e97d26cf15b4a45f0a59e9c
keita1899 <saboten18k@gmail.com>
2025-10-27 20:30:18 +0900

    feat: ローディングコンポーネントを作成

---
+++ frontend/src/components/ui/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="size-10 animate-spin rounded-full border-y-2 border-blue-500"></div>
+    </div>
+  )
+}
