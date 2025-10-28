681faedab71c8120291e22353ae0b5745a9535bf
keita1899 <saboten18k@gmail.com>
2025-10-28 21:31:51 +0900

    feat: Memoモデルとマイグレーションを作成し、マイグレーションを実行

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_20_095856) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_28_122111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_20_095856) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "date"], name: "index_daily_reports_on_user_id_and_date", unique: true
     t.index ["user_id"], name: "index_daily_reports_on_user_id"
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +64,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_20_095856) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "daily_reports", "users"
+  add_foreign_key "memos", "users"
 end
