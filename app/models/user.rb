44024eab11cd8d52535623e8f2faa13607d7258c
keita1899 <saboten18k@gmail.com>
2025-11-28 22:24:12 +0900

    fix: Deviseとの整合性のため、メールアドレスがないとGithubログインできないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
   def self.from_omniauth(auth)
+    return nil if auth.info.email.blank?
+
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
