243c8f9ce612d64dd8d0bbed47eef5f160087168
keita1899 <saboten18k@gmail.com>
2025-11-28 22:31:59 +0900

    refactor: Rubocopの警告のため、githubメソッドの処理を分割

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,13 +5,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user&.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      handle_persisted_user
     else
-      flash[:alert] = t('devise.omniauth_callbacks.failure.email_missing')
-      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url
+      handle_new_user
     end
+  end
+
+  private
+
+  def handle_persisted_user
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+  end
+
+  def handle_new_user
+    flash[:alert] = t('devise.omniauth_callbacks.failure.email_missing')
+    session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
+    redirect_to new_user_registration_url
   end
 
   def failure
