2bbd2b2b73e15298362f897a6261bfc243e68a2c
keita1899 <saboten18k@gmail.com>
2025-11-29 23:58:44 +0900

    feat: usersテーブルにnicknameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_25_113957) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_29_134425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_113957) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
