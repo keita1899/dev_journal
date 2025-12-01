5aefd47a74786a32f05a938b56ad3338d71f4921
keita1899 <saboten18k@gmail.com>
2025-12-01 12:13:26 +0900

    test:  guestのtrait、ゲストログインのモデルスペック、リクエストスペックを追加

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -5,28 +5,59 @@ RSpec.describe 'Sessions', type: :request do
 
   include Devise::Test::IntegrationHelpers
 
-  context 'ログイン済みの場合' do
-    before do
-      sign_in user
+  describe 'ログアウト' do
+    context 'ログイン済みの場合' do
+      before do
+        sign_in user
+      end
+
+      it 'DELETEリクエストによりログアウトが成功し、ルートパスにリダイレクトされること' do
+        delete destroy_user_session_path
+
+        expect(response).to have_http_status(:see_other)
+        expect(response).to redirect_to(root_path)
+
+        follow_redirect!
+        expect(response.body).to include('ログアウトしました。')
+      end
     end
 
-    it 'DELETEリクエストによりログアウトが成功し、ルートパスにリダイレクトされること' do
-      delete destroy_user_session_path
+    context '未ログインの場合' do
+      it 'ログアウト処理を実行してもエラーにならないこと' do
+        delete destroy_user_session_path
 
-      expect(response).to have_http_status(:see_other)
-      expect(response).to redirect_to(root_path)
-
-      follow_redirect!
-      expect(response.body).to include('ログアウトしました。')
+        follow_redirect!
+        expect(response.body).to include('既にログアウト済みです。')
+      end
     end
   end
 
-  context '未ログインの場合' do
-    it 'ログアウト処理を実行してもエラーにならないこと' do
-      delete destroy_user_session_path
+  describe 'ゲストログイン' do
+    context 'ユーザーが未登録の場合' do
+      it 'ゲストログインが成功し、フラッシュメッセージが表示されること' do
+        expect { post users_guest_sign_in_path }.to change(User, :count).by(1)
+        follow_redirect!
+        expect(response.body).to include(I18n.t('devise.sessions.guest_welcome'))
+        expect(User.last.email).to eq 'guest@example.com'
+        expect(User.last.nickname).to eq 'ゲスト'
+      end
+    end
 
-      follow_redirect!
-      expect(response.body).to include('既にログアウト済みです。')
+    context 'ゲストユーザーが既に存在する場合' do
+      let(:existing_guest) { create(:user, :guest) }
+      let(:initial_user_count) { User.count }
+
+      before do
+        delete destroy_user_session_path
+        post users_guest_sign_in_path
+        follow_redirect!
+      end
+
+      it '新規ユーザーを作成せず、既存のユーザーでログインすること' do
+        expect(User.count).to eq initial_user_count
+
+        expect(response.body).to include(I18n.t('devise.sessions.guest_welcome'))
+      end
     end
   end
 end
