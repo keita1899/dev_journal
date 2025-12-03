0bd6c93052604dd56705ca0aeb3bcb9c952e9137
keita1899 <saboten18k@gmail.com>
2025-12-03 23:16:58 +0900

    fix: 未ログイン時にトップページにリダイレクトされるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def authenticate_user!(_options = {})
+    return if user_signed_in?
+
+    flash[:alert] = t('common.login_required')
+
+    redirect_to root_path and return
+  end
 end
