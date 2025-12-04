67fc37d7912040d6ee52a16bde07ed0c2d819fb4
keita1899 <saboten18k@gmail.com>
2025-12-04 21:34:50 +0900

    fix: ファイルの読込時の日付で計算されてしまうため、実行時の日付で計算されるように修正

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -1,6 +1,8 @@
 class DailyReport < ApplicationRecord
   belongs_to :user
 
-  validates :date, presence: true, comparison: { less_than_or_equal_to: Date.current }, uniqueness: { scope: :user_id }
+  validates :date, presence: true, comparison: { less_than_or_equal_to: lambda {
+    Date.current
+  } }, uniqueness: { scope: :user_id }
   validates :content, presence: true
 end
