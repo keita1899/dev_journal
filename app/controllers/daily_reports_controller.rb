29a7585cc80dc358c830e6b9154d4d500315e880
keita1899 <saboten18k@gmail.com>
2025-12-09 16:18:51 +0900

    feat: indexアクションにカレンダーで表示している月の日報を取得する処理を追加
    - 日報がある場合は編集ページへのリンクを、ない場合は作成ページへのリンクを作成するため

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,7 +1,15 @@
 class DailyReportsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    start_date = params[:start_date]&.to_date || Date.current
+    start_of_data_month = start_date.beginning_of_month
+    end_of_data_month = start_date.end_of_month
+    @daily_reports = current_user.daily_reports
+                                 .select(:id, :date, :user_id)
+                                 .where(date: start_of_data_month..end_of_data_month)
+                                 .order(date: :asc)
+  end
 
   def new
     @daily_report = current_user.daily_reports.build(date: Date.current)
