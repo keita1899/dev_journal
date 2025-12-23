092aa94267a6e5ded2d2b441d7bcc366f578b079
keita1899 <saboten18k@gmail.com>
2025-12-23 12:17:59 +0900

    test: Memoモデルとコントローラーのスペック追加
    - モデルスペック: バリデーション、アソシエーション、default_scope
    - リクエストスペック: index, createアクションのテスト
    - FactoryBotのファクトリー追加

---
+++ spec/models/memo_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Memo, type: :model do
+  describe 'Validation' do
+    let(:user) { create(:user) }
+
+    context '正常系' do
+      it 'すべての属性が正しければ有効であること' do
+        memo = build(:memo, user: user)
+        expect(memo).to be_valid
+      end
+
+      it '本文が500文字以内であれば有効であること' do
+        memo = build(:memo, user: user, body: 'a' * 500)
+        expect(memo).to be_valid
+      end
+    end
+
+    context '異常系' do
+      it { is_expected.to validate_presence_of(:body) }
+      it { is_expected.to validate_length_of(:body).is_at_most(500) }
+    end
+  end
+
+  describe 'Association' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'default_scope' do
+    let(:user) { create(:user) }
+    let!(:old_memo) { create(:memo, user: user, created_at: 1.day.ago) }
+    let!(:new_memo) { create(:memo, user: user, created_at: 1.hour.ago) }
+
+    it '作成日時の降順で取得されること' do
+      expect(Memo.all).to eq([new_memo, old_memo])
+    end
+  end
+end
