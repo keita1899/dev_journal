d5fd6e574ff60ff4077c9850142923967ff6bf1b
keita1899 <saboten18k@gmail.com>
2025-12-23 12:17:26 +0900

    feat: メモ作成用のStimulusコントローラー追加
    - モーダル表示制御用のmodal_controller
    - 文字数カウンター用のcharacter_counter_controller

---
+++ app/javascript/controllers/character_counter_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ['input', 'counter', 'remaining']
+  static values = { max: Number }
+
+  connect() {
+    this.updateCounter()
+  }
+
+  updateCounter() {
+    const currentLength = this.inputTarget.value.length
+    const remaining = this.maxValue - currentLength
+
+    this.remainingTarget.textContent = remaining
+
+    if (remaining < 0) {
+      this.counterTarget.classList.add('text-red-500')
+      this.counterTarget.classList.remove('text-gray-500')
+      this.element.setAttribute('maxlength', currentLength)
+    } else {
+      this.counterTarget.classList.remove('text-red-500')
+      this.counterTarget.classList.add('text-gray-500')
+      this.element.setAttribute('maxlength', this.maxValue)
+    }
+  }
+}
